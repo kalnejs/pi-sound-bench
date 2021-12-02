168be50d61fbc3b30ecd51ba3b13e8df23828177
klavs <klavs.kalnejs@gmain.com>
2021-12-02 12:57:01 +0200

    init

--- pi-sound-bench.py
+++ pi-sound-bench.py
@@ -8,20 +8,20 @@ import time
 
 class GPIO:
 
-    BASE_PATH          = "/sys/class/gpio"
-    EXPORT_PATH        = BASE_PATH + "/export"
+    BASE_PATH          = '/sys/class/gpio'
+    EXPORT_PATH        = BASE_PATH + '/export'
 
-    PATH               = BASE_PATH + "/gpio%d"
-    DIRECTION_PATH     = PATH + "/direction"
-    EDGE_PATH          = PATH + "/edge"
-    VALUE_PATH         = PATH + "/value"
+    PATH               = BASE_PATH + '/gpio%d'
+    DIRECTION_PATH     = PATH + '/direction'
+    EDGE_PATH          = PATH + '/edge'
+    VALUE_PATH         = PATH + '/value'
 
-    EDGE_NONE          = "none"
-    EDGE_RISING        = "rising"
-    EDGE_FALLING       = "falling"
+    EDGE_NONE          = 'none'
+    EDGE_RISING        = 'rising'
+    EDGE_FALLING       = 'falling'
 
-    DIRECTION_IN       = "in"
-    DIRECTION_OUT      = "out"
+    DIRECTION_IN       = 'in'
+    DIRECTION_OUT      = 'out'
 
     DIRECTIONS = (DIRECTION_IN, DIRECTION_OUT)
     EDGES = (EDGE_NONE, EDGE_RISING, EDGE_FALLING)
@@ -44,7 +44,7 @@ class GPIO:
         with open(GPIO.DIRECTION_PATH % self.number, 'w') as file:
                     file.write(direction)
 
-        if(edge != "none"):
+        if(edge != GPIO.EDGE_NONE):
             with open(GPIO.EDGE_PATH, 'w') as file:
                 file.write(edge)  
 
@@ -55,54 +55,54 @@ class GPIO:
 
     def set(self, value):
         if(value):
-            self.value_file.write("1")
+            self.value_file.write('1')
             self.value_file.seek(0)
         else:
-            self.value_file.write("0")
+            self.value_file.write('0')
             self.value_file.seek(0)           
 
     def event_loop(self):
-        print("GPIO: starting event_loop")
+        print('GPIO: starting event_loop')
 
         while True:
             events = self.epoll.poll(1)
             for fileno, event in events:
                 if fileno == self.value_file.fileno():
-                    print("shitme")
+                    print('shitme')
 
 class StoragePlayer:
 
-    MEDIA_BASE_PATH = "/media/"
+    MEDIA_BASE_PATH = '/media/'
     MEDIA_AUDIO_EXTENSIONS = ['.mp3', '.wav']
 
     def __init__(self):
-        media_path = StoragePlayer.MEDIA_BASE_PATH + os.getlogin() + "/"
+        media_path = StoragePlayer.MEDIA_BASE_PATH + os.getlogin() + '/'
         media_list = os.listdir(media_path)
 
         if(not len(media_list)):
-            self.base_path = ""
+            self.base_path = ''
             return
 
         self.base_path = media_path + media_list[0]
 
-    def get_audio_list(self, base_folder=""):
+    def get_audio_list(self, base_folder=''):
 
         if(not self.base_path):
-            print("StoragePlayer: No media detected...")
+            print('StoragePlayer: No media detected...')
             return []
 
         list = os.listdir(self.base_path + base_folder)
 
-        file_list = filter(lambda item: os.path.isfile(self.base_path + base_folder+ "/" + item), list)
+        file_list = filter(lambda item: os.path.isfile(self.base_path + base_folder+ '/' + item), list)
 
         if(not len(file_list)):
-            print("StoragePlayer: No files detected in path...")
+            print('StoragePlayer: No files detected in path...')
             return []
 
         audio_list = filter(lambda item: [ele for ele in StoragePlayer.MEDIA_AUDIO_EXTENSIONS if(ele in item)], file_list)
 
         if(not len(audio_list)):
-            print("StoragePlayer: No audio files detected in path...")
+            print('StoragePlayer: No audio files detected in path...')
             return []
 
         return audio_list 
